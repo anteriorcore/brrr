797331b60df44cae98eb2cfcd4fb82ac97762f88
Shun Ueda <me@shu.nu>
2026-01-30 16:35:02 -0500

    feat: app implementation

--- typescript/src/codec.ts
+++ typescript/src/codec.ts
@@ -1,11 +1,12 @@
+import type { Task } from "./app.ts";
 import type { Call } from "./call.ts";
 
-export interface Codec {
+export interface Codec<C> {
   encodeCall<A extends unknown[]>(taskName: string, args: A): Promise<Call>;
 
   invokeTask<A extends unknown[], R>(
     call: Call,
-    task: (...args: A) => Promise<R>,
+    task: Task<C, A, R>,
   ): Promise<Uint8Array>;
 
   decodeReturn(taskName: string, payload: Uint8Array): unknown;
