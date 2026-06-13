80c7ee42cd290b59d6f6c0afb36567282045cffe
Robin <robin.lewis@anterior.com>
2026-06-12 22:11:29 -0400

    test(docsync): test docsync-get

--- docsync/package.nix
+++ docsync/package.nix
@@ -1,5 +1,7 @@
 {
+  diffutils,
   gnused,
+  jq,
   lib,
   package-lock2nix,
   runCommand,
@@ -8,6 +10,27 @@
 let
   final = package-lock2nix.mkNpmModule {
     src = ./.;
+    doInstallCheck = true;
+    nativeBuildInputs = [
+      diffutils
+      jq
+    ];
+    installCheckPhase =
+      let
+        fixtures = {
+          CmdCheck = "docsync-check checks if two directories' doc tags are in sync.";
+          CmdGet = "docsync-get extracts all docsync nodes under a path.";
+        };
+        fixture = builtins.toFile "fixture" (builtins.toJSON fixtures);
+      in
+      ''
+        f="$(mktemp)"
+        $out/bin/docsync-get ${./src} > $f
+        diff -u <(jq --sort-keys . ${fixture}) <(jq --sort-keys . $f)
+        $out/bin/docsync-get ${./src} CmdGet > $f
+        diff -u ${builtins.toFile "test" (fixtures.CmdGet + "\n")} $f
+        ! $out/bin/docsync-get ${./src} NonExistentKey
+      '';
     passthru.tests.docsync = runCommand "docsync" { nativeBuildInputs = [ final ]; } ''
       docsync-check ${../python/src} ${../typescript/src}
       touch $out
--- docsync/src/cmds.ts
+++ docsync/src/cmds.ts
@@ -4,6 +4,11 @@ import process from "node:process";
 
 import { PathParser } from "./path-parser.ts";
 
+/**
+ * docsync-check checks if two directories' doc tags are in sync.
+ *
+ * <docsync>CmdCheck</docsync>
+ */
 export async function docsyncCheck(): Promise<void> {
   const [dirA, dirB] = process.argv.slice(2);
   if (!dirA || !dirB) {
@@ -29,14 +34,41 @@ E.g.:
   deepStrictEqual(a, b);
 }
 
+/**
+ * docsync-get extracts all docsync nodes under a path.
+ *
+ * <docsync>CmdGet</docsync>
+ */
 export async function docsyncGet(): Promise<void> {
-  const path = process.argv[2];
+  const [path, slug] = process.argv.slice(2);
   if (!path) {
-    console.error("Usage: docsync-get <PATH>");
+    console.error(`
+Usage: docsync-get <PATH> [SLUG]
+
+Example:
+
+    $ docsync-get ./src/cmds.ts | jq
+    {
+      "CmdCheck": "docsync-check checks if two directories' doc tags are in sync.",
+      "CmdGet": "docsync-get extracts all docsync nodes under a path."
+    }
+
+    $ docsync-get ./src/cmds.ts CmdGet
+    docsync-get extracts all docsync nodes under a path.
+`);
     process.exit(1);
   }
 
   const parser = new PathParser();
-  const o = Object.fromEntries((await parser.getPath(path)).entries());
-  console.log(JSON.stringify(o));
+  const m = await parser.getPath(path);
+  if (slug !== undefined) {
+    if (!m.has(slug)) {
+      console.error(`No such key ${slug} in docsync tags for ${path}`);
+      process.exit(1);
+    }
+    console.log(m.get(slug));
+  } else {
+    const o = Object.fromEntries(m.entries());
+    console.log(JSON.stringify(o));
+  }
 }
